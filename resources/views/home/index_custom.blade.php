f0d2830b80bb382c37bbbeb8e52a6991df2dc5a5
Lam Truong <lam.truong@miracles.vn>
2020-01-06 21:55:55 +0700

    update

--- resources/views/home/index_custom.blade.php
+++ resources/views/home/index_custom.blade.php
@@ -31,7 +31,7 @@
          var js, fjs = d.getElementsByTagName(s)[0];
          if (d.getElementById(id)) return;
          js = d.createElement(s); js.id = id;
-         js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+         js.src = '../js/fb.js';
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));</script>
  
