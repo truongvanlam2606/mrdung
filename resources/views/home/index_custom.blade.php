1ff3b032e2302e7c852862bbc78fa3c995dc589b
Lam Truong <lam.truong@miracles.vn>
2020-01-06 17:08:24 +0700

    update

--- resources/views/home/index_custom.blade.php
+++ resources/views/home/index_custom.blade.php
@@ -240,6 +240,33 @@
 
 
         {{-- end register code --}}
+        
+        <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v5.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="1618735408372301"
+  theme_color="#0084ff"
+  logged_in_greeting="Xin chào, mình có thể giúp gì cho bạn?"
+  logged_out_greeting="Xin chào, mình có thể giúp gì cho bạn?">
+      </div>
 
         {{-- isoical --}}
         <div class="isoical">
@@ -276,15 +303,15 @@
             </div>
         </div>
         {{-- end footer --}}
-        <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+        {{-- <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
 
-        <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+        <script src="https://sp.zalo.me/plugins/sdk.js"></script> --}}
 
 
 
     </div>
             {{--  Chat--}}
-            <div class="chat_contact">
+            {{-- <div class="chat_contact">
                 <div class="chat_c">
                     <button  class="chat_c_b" data-toggle="modal" data-target="#myModal">Ngoại tuyến</button>
                 </div>
@@ -333,7 +360,7 @@
                     </div>
                 </div>
             </div>
-    
+     --}}
             {{--  --}}
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
