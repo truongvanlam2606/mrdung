7e40361d81e2d47fe0e3bbc3c13109f0d809017c
Lam Truong <lam.truong@miracles.vn>
2020-01-09 02:39:01 +0700

    updare mes

--- resources/views/home/index_custom.blade.php
+++ resources/views/home/index_custom.blade.php
@@ -303,7 +303,8 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        // js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));</script>
 
